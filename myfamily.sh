f22a39400420f70d0bd0b096c9d0acd84e5eab81
Creata21 <yerezhepabdulla@gmail.com>
2022-08-02 13:02:30 +0600

    -r options removed

--- myfamily.sh
+++ myfamily.sh
@@ -1,3 +1,3 @@
-
-RELATIVES=`curl -s 'https://01.alem.school/assets/superhero/all.json' | jq -r '.[] | select(.id == '$HERO_ID') | .connections | .relatives'`
+HERO_ID=70
+RELATIVES=$(curl -s 'https://01.alem.school/assets/superhero/all.json' | jq '.[] | select(.id == '$HERO_ID') | .connections | .relatives')
 echo $RELATIVES
