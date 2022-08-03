7cc49e236ebbd0b068657337d90ce711f7263a57
Creata21 <yerezhepabdulla@gmail.com>
2022-08-03 21:06:54 +0600

    some change

--- printcomb.go
+++ printcomb.go
@@ -4,8 +4,8 @@ import "github.com/01-edu/z01"
 
 func PrintComb() {
 	for i := '0'; i < '9'; i++ {
-		for j := i + 1; j < '9'; j++ {
-			for x := j + 1; x < '9'; x++ {
+		for j := 'i' + 1; j < '9'; j++ {
+			for x := 'j' + 1; x < '9'; x++ {
 				if i < 7 {
 					z01.PrintRune(i)
 					if j < 8 {
