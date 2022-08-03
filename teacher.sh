50da74373898e09b2a2ca3f73a7c6dc66bbec959
Creata21 <yerezhepabdulla@gmail.com>
2022-08-03 14:22:46 +0600

    new file teacher.sh added

--- teacher.sh
+++ teacher.sh
@@ -1,4 +1,4 @@
-INTERVIEW_NUMBER=$(head -n 179 streets/Buckingham_street | tail -n 1 | cut -c 16-)
+INTERVIEW_NUMBER=$(head -n 179 streets/Buckingham_Place | tail -n 1 | cut -c 16-)
 echo $INTERVIEW_NUMBER
 echo $(find ./interviews/ -type f -name "*$INTERVIEW_NUMBER*" -exec cat {} \; -print)
 export MAIN_SUSPECT
