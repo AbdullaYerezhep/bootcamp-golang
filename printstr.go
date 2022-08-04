4e991b835507492d035bfbcfd6d1a49564752faf
Creata21 <yerezhepabdulla@gmail.com>
2022-08-04 20:51:22 +0600

    initial printstr.go added

---
+++ printstr.go
@@ -0,0 +1,11 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintStr(s string) {
+	chars := []rune(s)
+	for i := 0; i < len(chars); i++ {
+		z01.PrintRune(chars[i])
+	}
+	z01.PrintRune('\n')
+}
