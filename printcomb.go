1d73ad5363ddeac1208d79a4ecdc2cdbf1abb20e
Creata21 <yerezhepabdulla@gmail.com>
2022-08-04 12:48:28 +0600

    format fixed

--- printcomb.go
+++ printcomb.go
@@ -2,18 +2,17 @@ package piscine
 
 import "github.com/01-edu/z01"
 
-
 func PrintComb() {
 	for i := '0'; i <= '7'; i++ {
 		for j := i + 1; j <= '8'; j++ {
 			for x := j + 1; x <= '9'; x++ {
-					z01.PrintRune(i)
-					z01.PrintRune(j)
-					z01.PrintRune(x)
-					if i != '7' {
-						z01.PrintRune(',')
-						z01.PrintRune(' ')
-					}
+				z01.PrintRune(i)
+				z01.PrintRune(j)
+				z01.PrintRune(x)
+				if i != '7' {
+					z01.PrintRune(',')
+					z01.PrintRune(' ')
+				}
 			}
 		}
 	}
