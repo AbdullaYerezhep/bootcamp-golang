326ec8720249a473e157e8e2dee8590b6fa0b254
Creata21 <yerezhepabdulla@gmail.com>
2022-08-08 15:05:05 +0600

    if else logic modified

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -2,12 +2,12 @@ package piscine
 
 func IterativeFactorial(nb int) int {
 	n := 1
-	if nb < 64 && nb > 0 {
+	if nb > 64 && nb <= 0 {
+		return 0
+	} else {
 		for i := nb; i > 0; i-- {
 			n = n * i
 		}
 		return n
-	} else {
-		return 0
 	}
 }
