61d4160e520470f9a67ae04eb4a8313c21d19e55
Creata21 <yerezhepabdulla@gmail.com>
2022-08-09 19:32:57 +0600

    format fixed

--- index.go
+++ index.go
@@ -3,13 +3,13 @@ package piscine
 func Index(s string, toFind string) int {
 	if len(s) == 0 {
 		return 0
-	}else if len(toFind) == 0{
+	} else if len(toFind) == 0 {
 		return 0
 	}
-	q:=0
+	q := 0
 	for i := range s {
 		if s[i] == toFind[q] {
-			if q == len(toFind) -1 {
+			if q == len(toFind)-1 {
 				return i - (len(toFind) - 1)
 			}
 			q++
