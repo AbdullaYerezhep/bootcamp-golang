37a75cf3942bc21eaf771e18a4af3d047a2fa7b5
Creata21 <yerezhepabdulla@gmail.com>
2022-08-09 15:26:26 +0600

    format modified

--- isprime.go
+++ isprime.go
@@ -1,11 +1,11 @@
-package piscine 
+package piscine
 
-func IsPrime(nb int) bool{
-	if nb < 0{
+func IsPrime(nb int) bool {
+	if nb < 0 {
 		return false
 	}
-	for i := 2; i < nb; i++{
-		if	nb % i == 0 {
+	for i := 2; i < nb; i++ {
+		if nb%i == 0 {
 			return false
 		}
 	}
