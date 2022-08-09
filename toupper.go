c088789fbcf1bb8a7ae294564d39e8fe5580e8ab
Creata21 <yerezhepabdulla@gmail.com>
2022-08-09 20:38:05 +0600

    format modified

--- toupper.go
+++ toupper.go
@@ -4,7 +4,7 @@ func ToUpper(s string) string {
 	runes := []rune(s)
 	var upperrunes []rune
 	for i := 0; i < len(s); i++ {
-		if int(runes[i]) > 96 && int(runes[i]) < 123{
+		if int(runes[i]) > 96 && int(runes[i]) < 123 {
 			upperrunes = append(upperrunes, rune(int(runes[i])-32))
 		} else {
 			upperrunes = append(upperrunes, rune(int(runes[i])))
