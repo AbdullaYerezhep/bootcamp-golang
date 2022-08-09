e937fc11ad230af5b8cde4fa958790c58742be81
Creata21 <yerezhepabdulla@gmail.com>
2022-08-09 16:30:16 +0600

    initial findnextprime func commit

---
+++ findnextprime.go
@@ -0,0 +1,28 @@
+package piscine
+
+func FindNextPrime(nb int) int {
+	result := 1
+	if nb <= 0 || nb == 1 {
+		result = 0
+	}
+	for i := 2; i < nb; i++ {
+		if nb%i == 0 {
+			result = 0
+		}
+	}
+
+	if result == 0 {
+		nb++
+		for j := 2; j < nb; j++ {
+			if nb%j == 0 {
+				nb++
+				j = 2
+			}
+		}
+		result = nb
+	} else {
+		result = nb
+	}
+
+	return result
+}
