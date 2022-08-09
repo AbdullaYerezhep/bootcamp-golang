630a1d64c5f4ccc25fa33adc4a3b950e397883f6
Creata21 <yerezhepabdulla@gmail.com>
2022-08-09 16:54:22 +0600

    recursve method was applied

--- findnextprime.go
+++ findnextprime.go
@@ -1,28 +1,15 @@
 package piscine
 
 func FindNextPrime(nb int) int {
-	result := 1
 	if nb <= 0 || nb == 1 {
-		result = 2
-	} else {
-		for i := 2; i < nb; i++ {
-			if nb%i == 0 {
-				result = 0
-			}
-		}
+		return 2
+	}
 
-		if result == 0 {
-			nb++
-			for j := 2; j < nb; j++ {
-				if nb%j == 0 {
-					nb++
-					j = 2
-				}
-			}
-			result = nb
-		} else {
-			result = nb
+	for i := 2; i < nb/2; i++ {
+		if nb%i == 0 {
+			return FindNextPrime(nb + 1)
 		}
 	}
-	return result
+
+	return nb
 }
