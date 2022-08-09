5648a9c0f71566e61afcf6550e39eb7c5b9dc85a
Creata21 <yerezhepabdulla@gmail.com>
2022-08-09 18:49:14 +0600

    format modified

--- alphacount.go
+++ alphacount.go
@@ -3,8 +3,8 @@ package piscine
 func AlphaCount(s string) int {
 	runes := []rune(s)
 	count := 0
-	for i:=0; i<len(s); i++ {
-		if(int(runes[i]) > 64 && int(runes[i]) < 91 ||  int(runes[i]) > 96 && int(runes[i]) < 123) {
+	for i := 0; i < len(s); i++ {
+		if int(runes[i]) > 64 && int(runes[i]) < 91 || int(runes[i]) > 96 && int(runes[i]) < 123 {
 			count++
 		}
 	}
