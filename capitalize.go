789889c9cbe156bb0ee8750dce54c4e006028f6c
Creata21 <yerezhepabdulla@gmail.com>
2022-08-10 18:21:13 +0600

    initial capitalize func

---
+++ capitalize.go
@@ -0,0 +1,22 @@
+package piscine
+
+func Capitalize(s string) string {
+	runes := []rune(s)
+	str := ""
+	j := 0
+	for i := 0; i < len(s); i++ {
+		j = i
+		if j-1 < 0 {
+			j = 1
+		}
+		if runes[i] >= 'a' && runes[i] <= 'z' && (runes[j-1] == ' ' || runes[j-1] == '+' || i == 0) {
+			runes[i] = rune(int(runes[i] - 32))
+		}
+	}
+
+	for i := 0; i < len(runes); i++ {
+		str += string(runes[i])
+	}
+
+	return str
+}
