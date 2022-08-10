bd818de553b142baaabd34da520bb1928e3f66bc
Creata21 <yerezhepabdulla@gmail.com>
2022-08-10 20:12:01 +0600

    initial printprogramname func

---
+++ printprogramname/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	programname := os.Args[1]
+	print(len(os.Args))
+	for i := 0; i < len(os.Args[0]); i++ {
+		z01.PrintRune(rune(programname[i]))
+	}
+}
