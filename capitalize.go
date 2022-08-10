509826916a35431b5fae790ac7d6259ff5624bef
Creata21 <yerezhepabdulla@gmail.com>
2022-08-10 19:26:25 +0600

    if else modified

--- capitalize.go
+++ capitalize.go
@@ -3,14 +3,17 @@ package piscine
 func Capitalize(s string) string {
 	runes := []rune(s)
 	str := ""
-	j := 0
+	isnew := true
 	for i := 0; i < len(s); i++ {
-		j = i
-		if j-1 < 0 {
-			j = 1
-		}
-		if runes[i] >= 'a' && runes[i] <= 'z' && ((runes[j-1] >= ' ' && runes[j-1] <= '/') || (runes[j-1] >= ':' && runes[j-1] <= '@')) || i == 0 {
-			runes[i] = rune(int(runes[i] - 32))
+		if runes[i] >= 'a' && runes[i] <= 'z' || runes[i] >= '0' && runes[i] <= '9' || runes[i] >= 'A' && runes[i] <= 'Z' {
+			if runes[i] >= 'A' && runes[i] <= 'Z' && !isnew {
+				runes[i] = rune(int(runes[i] + 32))
+			} else if runes[i] >= 'a' && runes[i] <= 'z' && isnew {
+				runes[i] = rune(int(runes[i] - 32))
+			}
+			isnew = false
+		} else {
+			isnew = true
 		}
 	}
 
