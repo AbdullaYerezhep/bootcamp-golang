7e34b3630e737e16e8787630e6a3484033ef29a9
Creata21 <yerezhepabdulla@gmail.com>
2022-08-10 19:37:59 +0600

    format modified

--- join.go
+++ join.go
@@ -1,11 +1,11 @@
 package piscine
 
 func Join(elems []string, sep string) string {
-	str:= ""
-	for i:=0; i < len(elems); i++{
+	str := ""
+	for i := 0; i < len(elems); i++ {
 		str += elems[i]
-		if i != len(elems)-1{
-		str += sep
+		if i != len(elems)-1 {
+			str += sep
 		}
 	}
 	return str
