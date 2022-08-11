cca42777fc29ac90077f3714952810d345b67eb0
Creata21 <yerezhepabdulla@gmail.com>
2022-08-11 13:30:32 +0600

    format modified

--- revparams/main.go
+++ revparams/main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	arguments := os.Args[1:]
-	for i := len(arguments)-1; i >= 0; i--{
+	for i := len(arguments) - 1; i >= 0; i-- {
 		for _, char := range arguments[i] {
 			z01.PrintRune(char)
 		}
