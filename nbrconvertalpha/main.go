36371b03a268ed027e1e79c3d0c9ac8de28dae55
Creata21 <yerezhepabdulla@gmail.com>
2022-08-11 18:53:29 +0600

    initial nbrconvertalpha func

---
+++ nbrconvertalpha/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	args := os.Args[1:]
+	isupper := false
+
+	if args[0] == "--upper" {
+		isupper = true
+		args = args[1:]
+	}
+	for i := 0; i < len(args); i++ {
+
+		if isupper && rune(StringToInt(args[i])+64) >= 'A' && rune(StringToInt(args[i])+64) <= 'Z' {
+			args[i] = string(rune(StringToInt(args[i]) + 64))
+		} else if isupper {
+			args[i] = string(' ')
+		}
+
+		if !isupper && rune(StringToInt(args[i])+96) >= 'a' && rune(StringToInt(args[i])+96) <= 'z' {
+			args[i] = string(rune(StringToInt(args[i]) + 96))
+		} else {
+			args[i] = string(' ')
+		}
+
+		for _, char := range args[i] {
+			z01.PrintRune(char)
+		}
+
+	}
+}
+
+func StringToInt(s string) int {
+	runes := []rune(s)
+
+	var arr []int
+
+	n := 0
+	for i := range s {
+		if runes[i] <= '9' && runes[i] >= '0' {
+			arr = append(arr, int(runes[i]-48))
+		}
+	}
+
+	for i := 0; i < len(arr); i++ {
+		n = n*10 + arr[i]
+	}
+
+	return n
+}
