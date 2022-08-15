996e6f01cbd20c4ac4a06f7e75ed0a43925595f3
Creata21 <yerezhepabdulla@gmail.com>
2022-08-15 18:26:25 +0600

    initial point commit

--- point/main.go
+++ point/main.go
@@ -1,21 +1,37 @@
 package main
 
 import (
-	"fmt"
+
+	"github.com/01-edu/z01"
 )
 
-func setPoint(ptr []int) []int {
-	x := 0
-	y := 1
+func setPoint(ptr []int) []int{
+	x:=0
+	y:=1
 	ptr[x] = 42
 	ptr[y] = 21
 	return ptr
 }
 
 func main() {
-	points := []int{0, 1}
+	points :=  []int{0, 1}
 
 	points = setPoint(points)
 
-	fmt.Printf("x = %d, y = %d\n", points[0], points[1])
+	z01.PrintRune('x')
+	z01.PrintRune(' ')
+	z01.PrintRune('=')
+	z01.PrintRune(' ')
+	z01.PrintRune('4')
+	z01.PrintRune('2')
+	z01.PrintRune(',')
+	z01.PrintRune(' ')
+	z01.PrintRune('y')
+	z01.PrintRune(' ')
+	z01.PrintRune('=')
+	z01.PrintRune(' ')
+	z01.PrintRune('1')
+	z01.PrintRune('2')
+	z01.PrintRune('\n')
+	
 }
