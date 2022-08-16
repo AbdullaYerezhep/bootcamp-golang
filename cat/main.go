d6ce73635467c06ff9638314d63fecf10b47c4cc
Creata21 <yerezhepabdulla@gmail.com>
2022-08-16 17:14:10 +0600

    initial cat commit

---
+++ cat/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"io"
+	"io/ioutil"
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	args := os.Args[1:]
+	if len(args) < 1 {
+		io.Copy(os.Stdout, os.Stdin)
+	} else {
+		for i := range args {
+			content, err := ioutil.ReadFile(args[i])
+			if err != nil {
+				printStr("ERROR: ")
+				printStr(err.Error())
+				z01.PrintRune('\n')
+			}
+			printStr(string(content))
+		}
+	}
+}
+
+func printStr(s string) {
+	for _, r := range s {
+		z01.PrintRune(r)
+	}
+}
--- point/main.go
+++ point/main.go
@@ -1,35 +1,14 @@
 package main
 
-import (
-	"github.com/01-edu/z01"
-)
-
-func setPoint(ptr []int) []int {
-	x := 0
-	y := 1
-	ptr[x] = 42
-	ptr[y] = 21
-	return ptr
+func setPoint(ptr *point) {
+	ptr.x = 42
+	ptr.y = 21
 }
 
 func main() {
-	points := []int{0, 1}
+	points := &point{}
 
-	points = setPoint(points)
+	setPoint(points)
 
-	z01.PrintRune('x')
-	z01.PrintRune(' ')
-	z01.PrintRune('=')
-	z01.PrintRune(' ')
-	z01.PrintRune('4')
-	z01.PrintRune('2')
-	z01.PrintRune(',')
-	z01.PrintRune(' ')
-	z01.PrintRune('y')
-	z01.PrintRune(' ')
-	z01.PrintRune('=')
-	z01.PrintRune(' ')
-	z01.PrintRune('2')
-	z01.PrintRune('1')
-	z01.PrintRune('\n')
+	fmt.Printf("x = %d, y = %d\n",points.x, points.y)
 }
