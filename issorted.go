0b819693b1cb362badf470b1c67629c82f7231bc
Creata21 <yerezhepabdulla@gmail.com>
2022-08-16 19:35:11 +0600

    if all elements of array are equal true is returned

--- issorted.go
+++ issorted.go
@@ -3,17 +3,20 @@ package piscine
 func IsSorted(f func(a, b int) int, a []int) bool {
 	ascending := 0
 	descending := 0
+	equal := 0
 	for i := range a {
 		if i != len(a)-1 {
 			if f(a[i], a[i+1]) < 0 {
 				ascending++
 			} else if f(a[i], a[i+1]) > 0 {
 				descending++
+			} else {
+				equal++
 			}
 		}
 	}
 
-	if ascending == len(a)-1 || descending == len(a)-1 {
+	if ascending == len(a)-1 || descending == len(a)-1 || equal == len(a)-1 {
 		return true
 	}
 	return false
