a74f068940c04f7a5053166eb469c4e5246461df
Creata21 <yerezhepabdulla@gmail.com>
2022-08-16 17:17:01 +0600

    initial cat commit

--- point/main.go
+++ point/main.go
@@ -1,14 +1,4 @@
 package main
 
-func setPoint(ptr *point) {
-	ptr.x = 42
-	ptr.y = 21
-}
-
 func main() {
-	points := &point{}
-
-	setPoint(points)
-
-	fmt.Printf("x = %d, y = %d\n",points.x, points.y)
 }
