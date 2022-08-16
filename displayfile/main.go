5cc9c3b52a1096af2a885e616f24039fe327289b
Creata21 <yerezhepabdulla@gmail.com>
2022-08-16 14:03:20 +0600

    initial displayfile commit

---
+++ displayfile/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	args := os.Args[1:]
+
+	if len(args) < 1 {
+		fmt.Println("File name missing")
+	} else if len(args) > 1 {
+		fmt.Println("Too many arguments")
+	} else {
+		content, err := ioutil.ReadFile(args[0])
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Printf("%s", content)
+	}
+}
