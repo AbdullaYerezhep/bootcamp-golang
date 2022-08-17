74607d6c6c13e4822e4ccc30bd5570346c25fc78
Creata21 <yerezhepabdulla@gmail.com>
2022-08-17 18:51:34 +0600

    initial advancedsortwordarr commit

---
+++ advancedsortwordarr.go
@@ -0,0 +1,11 @@
+package piscine
+
+func AdvancedSortWordArr(a []string, f func(a, b string) int) {
+	for i := len(a) - 1; i > 0; i-- {
+		for j := 0; j < i; j++ {
+			if a[j] > a[j+1] {
+				a[j], a[j+1] = a[j+1], a[j]
+			}
+		}
+	}
+}
