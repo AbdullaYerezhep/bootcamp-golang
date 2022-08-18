d39c8e689f428e7966b3287e69442c26a9b3262f
Creata21 <yerezhepabdulla@gmail.com>
2022-08-18 17:02:44 +0600

    initial enigma commit

---
+++ enigma.go
@@ -0,0 +1,13 @@
+package piscine
+
+func Enigma(a ***int, b *int, c *******int, d ****int) {
+	tempa := ***a
+	tempb := *b
+	tempc := *******c
+	tempd := ****d
+
+	*******c = tempa
+	****d = tempc
+	*b = tempd
+	***a = tempb
+}
