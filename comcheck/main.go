e187f5ea02028548c45de83bb15793e74b360135
Creata21 <yerezhepabdulla@gmail.com>
2022-08-18 15:02:34 +0600

    initial comcheck commit

---
+++ comcheck/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	args := os.Args[1:]
+	for i := range args {
+		if args[i] == "galaxy" || args[i] == "galaxy 01" || args[i] == "01" {
+			fmt.Println("Alert!!")
+			break
+		}
+	}
+}
