537c46ce4280705cc23c3fac94f5ff138fe9bd59
Creata21 <yerezhepabdulla@gmail.com>
2022-08-19 01:37:48 +0600

    initial bits  commit

--- activebits.go
+++ activebits.go
@@ -1,8 +1,18 @@
 package piscine
 
-func ActiveBits(n int) uint {
-	if 0 <= n && n < 2 {
-		return uint(n)
+func ActiveBits(n int) int {
+	array := []int{}
+
+	for n != 0 {
+
+		array = append(array, n%2)
+		n = n / 2
 	}
-	return (uint(n) % 2) + ActiveBits(n/2)
+	add := 0
+	for i := 0; i < len(array); i++ {
+		if array[i] == 1 {
+			add++
+		}
+	}
+	return add
 }
