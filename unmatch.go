c1140678c72e0a0c679add80527d6f7a43bacb46
Creata21 <yerezhepabdulla@gmail.com>
2022-08-19 07:45:48 +0600

    umn

--- unmatch.go
+++ unmatch.go
@@ -1,20 +1,19 @@
 package piscine
 
 func Unmatch(a []int) int {
-	for i := 0; i < len(a)-1; i++ {
-		for j := 0; j < len(a)-1; j++ {
-			if j == i && j != len(a)-1 {
-				j++
-			}
+	var count int
+	for i := 0; i < len(a); i++ {
+		count = 0
+		for j := 0; j < len(a); j++ {
 			if a[i] == a[j] {
-				a[i] = a[len(a)-1]
-				a[len(a)-1] = 0
-				a = a[:len(a)-1]
+				count++
 			}
-			if len(a) == 1 {
-				break
+			if count%2 != 0 {
+				return a[i]
 			}
+
 		}
+
 	}
-	return a[0]
+	return -1
 }
