778cbef8c0113d33412ea8e2e11a82c4e3696b9c
Creata21 <yerezhepabdulla@gmail.com>
2022-08-24 17:54:38 +0600

    initial listpushfront.go

---
+++ listpushfront.go
@@ -0,0 +1,13 @@
+package piscine
+
+func ListPushFront(l *List, data interface{}) {
+	n := &NodeL{Data: data}
+
+	if l.Tail == nil {
+		l.Tail = n
+		l.Head = n
+	} else {
+		n.Next = l.Head
+		l.Head = n
+	}
+}
