96f47ad869c6493f4e76dffaca59372a803b6cf9
Creata21 <yerezhepabdulla@gmail.com>
2022-08-26 05:45:28 +0600

    struct added

--- sortedlistmerge.go
+++ sortedlistmerge.go
@@ -1,5 +1,11 @@
 package piscine
 
+type NodeI struct {
+	Data int
+	Next *NodeI
+}
+
+
 func SortedListMerge(n1 *NodeI, n2 *NodeI) *NodeI {
 	for i := n2; i != nil; i = i.Next {
 		n1 = SortListInsert(n1, i.Data)
